760ef43ba171dfe8d78388970b5e40c7c6368505
Micheal-adel98 <michealadell98@gmail.com>
2021-12-06 05:41:36 +0200

    extra images api

--- crowdfund/projectapi/serializers.py
+++ crowdfund/projectapi/serializers.py
@@ -3,6 +3,7 @@ from projects.models import Project
 
 
 class Projectser(serializers.ModelSerializer):
+    extra_images = serializers.StringRelatedField(many=True)
     class Meta:
         model = Project
         fields = '__all__'
